5f48d41c3816849f410959bd6ee6e480a5107185
Andrew McClenaghan <andymac4182@users.noreply.github.com>
2018-02-25 11:30:44 +1000

    Initial work on API Key management

---
+++ src/SeqCli/Cli/Commands/ApiKey/ListCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SeqCli.Cli.Features;
+using SeqCli.Connection;
+using Serilog;
+
+namespace SeqCli.Cli.Commands.ApiKey
+{
+    [Command("apikey", "list", "Send a structured log event to the server", Example =
+        "seqcli log -m 'Hello, {Name}!' -p Name=World -p App=Test")]
+    class ListCommand : Command
+    {
+        private readonly SeqConnectionFactory _connectionFactory;
+        private readonly ConnectionFeature _connection;
+
+        public ListCommand(SeqConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+            _connection = Enable<ConnectionFeature>();
+        }
+
+        protected override async Task<int> Run()
+        {
+            var connection = _connectionFactory.Connect(_connection);
+
+            var apiKeys = await connection.ApiKeys.ListAsync();
+            Log.Debug("Retrieved ApiKeys {@ApiKeys}", apiKeys);
+            var data = apiKeys.Select(a => new
+            {
+                a.AppliedProperties,
+                a.CanActAsPrincipal,
+                a.InputFilter,
+                a.Title,
+                a.Token,
+                a.UseServerTimestamps,
+                a.MinimumLevel,
+                a.IsDefault
+            });
+            foreach (var apiKey in data)
+            {
+                var apiKeyString = JsonConvert.SerializeObject(apiKey);
+
+                Console.WriteLine(apiKeyString);
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/SeqCli/Cli/Commands/ApiKey/RemoveCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seq.Api.Model.Inputs;
+using SeqCli.Cli.Features;
+using SeqCli.Connection;
+
+namespace SeqCli.Cli.Commands.ApiKey
+{
+    [Command("apikey", "remove", "Send a structured log event to the server", Example =
+        "seqcli log -m 'Hello, {Name}!' -p Name=World -p App=Test")]
+    class RemoveCommand : Command
+    {
+        private readonly SeqConnectionFactory _connectionFactory;
+        private readonly ConnectionFeature _connection;
+        private string _title;
+
+        public RemoveCommand(SeqConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+            _connection = Enable<ConnectionFeature>();
+            Options.Add(
+                "t=|title=",
+                "",
+                (t) => _title = t);
+        }
+
+        protected override async Task<int> Run()
+        {
+            var connection = _connectionFactory.Connect(_connection);
+
+            var apiKeys = await connection.ApiKeys.ListAsync();
+            var apiKeyToRemove = apiKeys.FirstOrDefault(ak => ak.Title == _title);
+            if (apiKeyToRemove == null)
+            {
+                Console.WriteLine($"\"{_title}\" API Key doesn't exist");
+                return -1;
+            }
+
+            await connection.ApiKeys.RemoveAsync(apiKeyToRemove);
+            Console.WriteLine($"\"{_title}\" API Key removed");
+            return 0;
+        }
+    }
+}
