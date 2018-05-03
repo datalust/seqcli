16b4631710253512ac1847b6669ccd586fc968b5
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-05-03 20:44:17 +1000

    Signal list and remove commands

--- src/SeqCli/Cli/Commands/ApiKey/RemoveCommand.cs
+++ src/SeqCli/Cli/Commands/ApiKey/RemoveCommand.cs
@@ -1,60 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright 2018 Datalust Pty Ltd and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Seq.Api.Model.Inputs;
 using SeqCli.Cli.Features;
 using SeqCli.Connection;
+using Serilog;
 
 namespace SeqCli.Cli.Commands.ApiKey
 {
     [Command("apikey", "remove", "Remove an API key from the server",
-        Example="seqcli apikey remove -t TestApiKey")]
+        Example="seqcli apikey remove -t 'Test API Key'")]
     class RemoveCommand : Command
     {
         readonly SeqConnectionFactory _connectionFactory;
+
+        readonly EntityIdentityFeature _entityIdentity;
         readonly ConnectionFeature _connection;
-        string _title;
-        string _id;
 
         public RemoveCommand(SeqConnectionFactory connectionFactory)
         {
-            _connectionFactory = connectionFactory;
-            _connection = Enable<ConnectionFeature>();
-            Options.Add(
-                "t=|title=",
-                "Remove API Keys with the specified title",
-                (t) => _title = t);
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
 
-            Options.Add(
-                "i=|id=",
-                "Remove API Keys with the specified Id",
-                (t) => _id = t);
+            _entityIdentity = Enable(new EntityIdentityFeature("API key", "remove"));
+            _connection = Enable<ConnectionFeature>();
         }
 
         protected override async Task<int> Run()
         {
-            if (_title != default && _id != default)
+            if (_entityIdentity.Title == null && _entityIdentity.Id == null)
             {
-                Console.WriteLine("You can only specify \"title\" or \"id\" not both");
+                Log.Error("A `title` or `id` must be specified");
                 return -1;
             }
 
             var connection = _connectionFactory.Connect(_connection);
 
-            var apiKeys = await connection.ApiKeys.ListAsync();
-            var apiKeyToRemove = apiKeys.Where(ak => ak.Title == _title || ak.Id == _id).ToList();
-            if (!apiKeyToRemove.Any())
+            var toRemove = _entityIdentity.Id != null ?
+                new[] {await connection.ApiKeys.FindAsync(_entityIdentity.Id)} :
+                (await connection.ApiKeys.ListAsync())
+                    .Where(ak => _entityIdentity.Title == ak.Title) 
+                    .ToArray();
+
+            if (!toRemove.Any())
             {
-                Console.WriteLine($"\"{_title}\" API Key doesn't exist");
+                Log.Error("No matching API key was found");
                 return -1;
             }
 
-            foreach (var apiKeyEntity in apiKeyToRemove)
-            {
+            foreach (var apiKeyEntity in toRemove)
                 await connection.ApiKeys.RemoveAsync(apiKeyEntity);
-            }
+
             return 0;
         }
     }
---
+++ src/SeqCli/Cli/Features/EntityIdentityFeature.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2018 Datalust Pty Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace SeqCli.Cli.Features
+{
+    class EntityIdentityFeature : CommandFeature
+    {
+        readonly string _entityName;
+        readonly string _verb;
+
+        string _title;
+        string _id;
+
+        public EntityIdentityFeature(string entityName, string verb)
+        {
+            _entityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
+            _verb = verb ?? throw new ArgumentNullException(nameof(verb));
+        }
+
+        public override void Enable(OptionSet options)
+        {
+            options.Add(
+                "t=|title=",
+                $"The title of the {_entityName}(s) to {_verb}",
+                t => _title = t);
+
+            options.Add(
+                "i=|id=",
+                $"The id of a single {_entityName} to {_verb}",
+                t => _id = t);
+        }
+
+        public override IEnumerable<string> GetUsageErrors()
+        {
+            if (Title != null && Id != null)
+                yield return "Only one of either `title` or `id` can be specified";
+        }
+
+        public string Title => string.IsNullOrWhiteSpace(_title) ? null : _title.Trim();
+
+        public string Id => string.IsNullOrWhiteSpace(_id) ? null : _id.Trim();
+    }
+}
