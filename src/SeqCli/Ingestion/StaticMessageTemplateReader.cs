55941fdada2e5be6ea054b347f1bc3069dc35a17
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-04-02 13:39:30 +1000

    Fixes #100 - -m and -l syntax for overriding message and level on ingestion

---
+++ src/SeqCli/Ingestion/StaticMessageTemplateReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog.Events;
+using Serilog.Parsing;
+
+namespace SeqCli.Ingestion
+{
+    class StaticMessageTemplateReader : ILogEventReader
+    {
+        readonly ILogEventReader _inner;
+        readonly MessageTemplate _messageTemplate;
+
+        public StaticMessageTemplateReader(ILogEventReader inner, string messageTemplate)
+        {
+            _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+            _messageTemplate = new MessageTemplateParser().Parse(messageTemplate);
+        }
+
+        public async Task<ReadResult> TryReadAsync()
+        {
+            var result = await _inner.TryReadAsync();
+
+            if (result.LogEvent == null)
+                return result;
+
+            var evt = new LogEvent(
+                result.LogEvent.Timestamp,
+                result.LogEvent.Level,
+                result.LogEvent.Exception,
+                _messageTemplate,
+                result.LogEvent.Properties.Select(kv => new LogEventProperty(kv.Key, kv.Value)));
+
+            return new ReadResult(evt, result.IsAtEnd);
+        }
+    }
+}
---
+++ test/SeqCli.Tests/Support/FixedLogEventReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using SeqCli.Ingestion;
+
+namespace SeqCli.Tests.Support
+{
+    class FixedLogEventReader : ILogEventReader
+    {
+        readonly ReadResult _result;
+
+        public FixedLogEventReader(ReadResult result)
+        {
+            _result = result;
+        }
+
+        public Task<ReadResult> TryReadAsync()
+        {
+            return Task.FromResult(_result);
+        }
+    }
+}
