c5c9e0a0505dfcd669c9f25291346b2a496cd307
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2020-02-26 14:21:13 +0100

    added create, remove and list commands for workspaces

---
+++ src/SeqCli/Cli/Commands/Workspace/CreateCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using SeqCli.Cli.Features;
+using SeqCli.Config;
+using SeqCli.Connection;
+using SeqCli.Util;
+
+namespace SeqCli.Cli.Commands.Workspace
+{
+    [Command("workspace", "create", "Create a workspace",
+       Example = "seqcli workspace create -t 'My Workspace'")]
+    class CreateCommand : Command
+    {
+        readonly SeqConnectionFactory _connectionFactory;
+
+        readonly ConnectionFeature _connection;
+        readonly OutputFormatFeature _output;
+
+        string _title, _description;
+        bool _isProtected;
+
+        public CreateCommand(SeqConnectionFactory connectionFactory, SeqCliConfig config)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+
+            Options.Add(
+                "t=|title=",
+                "A title for the workspace",
+                t => _title = ArgumentString.Normalize(t));
+
+            Options.Add(
+                "description=",
+                "A description for the workspace",
+                d => _description = ArgumentString.Normalize(d));
+
+            Options.Add(
+                "protected",
+                "Specify that the workspace is editable only by administrators",
+                _ => _isProtected = true);
+
+            _connection = Enable<ConnectionFeature>();
+            _output = Enable(new OutputFormatFeature(config.Output));
+        }
+
+        protected override async Task<int> Run()
+        {
+            var connection = _connectionFactory.Connect(_connection);
+
+            var workspace = await connection.Workspaces.TemplateAsync();
+
+            workspace.Title = _title;
+            workspace.Description = _description;
+            workspace.IsProtected = _isProtected;
+
+            workspace = await connection.Workspaces.AddAsync(workspace);
+
+            _output.WriteEntity(workspace);
+
+            return 0;
+        }
+    }
+}
---
+++ src/SeqCli/Cli/Commands/Workspace/RemoveCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SeqCli.Cli.Features;
+using SeqCli.Connection;
+using Serilog;
+
+namespace SeqCli.Cli.Commands.Workspace
+{
+    [Command("workspace", "remove", "Remove a workspace from the server",
+       Example = "seqcli workspace remove -t 'My Workspace'")]
+    class RemoveCommand : Command
+    {
+        readonly SeqConnectionFactory _connectionFactory;
+
+        readonly EntityIdentityFeature _entityIdentity;
+        readonly ConnectionFeature _connection;
+        readonly EntityOwnerFeature _entityOwner;
+
+        public RemoveCommand(SeqConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+
+            _entityIdentity = Enable(new EntityIdentityFeature("workspace", "remove"));
+            _entityOwner = Enable(new EntityOwnerFeature("workspace", "remove", _entityIdentity));
+            _connection = Enable<ConnectionFeature>();
+        }
+
+        protected override async Task<int> Run()
+        {
+            if (_entityIdentity.Title == null && _entityIdentity.Id == null)
+            {
+                Log.Error("A `title` or `id` must be specified");
+                return 1;
+            }
+
+            var connection = _connectionFactory.Connect(_connection);
+
+            var toRemove = _entityIdentity.Id != null ?
+                new[] { await connection.Workspaces.FindAsync(_entityIdentity.Id) } :
+                (await connection.Workspaces.ListAsync(ownerId: _entityOwner.OwnerId, shared: _entityOwner.IncludeShared))
+                    .Where(workspace => _entityIdentity.Title == workspace.Title)
+                    .ToArray();
+
+            if (!toRemove.Any())
+            {
+                Log.Error("No matching signal was found");
+                return 1;
+            }
+
+            foreach (var workspace in toRemove)
+                await connection.Workspaces.RemoveAsync(workspace);
+
+            return 0;
+        }
+    }
+}
