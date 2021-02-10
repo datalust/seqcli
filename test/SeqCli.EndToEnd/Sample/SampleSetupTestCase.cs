c093280cb0152ee5b2b317ed2816dea68793017b
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2021-02-10 16:52:36 +1000

    Fix test

--- test/SeqCli.EndToEnd/Sample/SampleSetupTestCase.cs
+++ test/SeqCli.EndToEnd/Sample/SampleSetupTestCase.cs
@@ -15,7 +15,7 @@ namespace SeqCli.EndToEnd.Sample
             Assert.Equal(0, exit);
 
             // Depends on the most other entities, so is a pretty good proxy for success.
-            var sampleWorkspace = (await connection.Workspaces.ListAsync())
+            var sampleWorkspace = (await connection.Workspaces.ListAsync(shared: true))
                 .SingleOrDefault(w => w.Title == "Sample");
 
             Assert.NotNull(sampleWorkspace);
