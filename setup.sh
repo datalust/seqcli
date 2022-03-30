09a56c2ebf1a78cb0b0675889bb6b22dd0b85cdb
Ashley Mannix <kodraus@hey.com>
2022-03-30 21:28:48 +1000

    enable more docker platforms

--- setup.sh
+++ setup.sh
@@ -13,3 +13,6 @@ curl https://dot.net/v1/dotnet-install.sh -sSfL --output dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --install-dir $HOME/.dotnetcli --no-path --version $RequiredDotnetVersion
 rm dotnet-install.sh
+
+docker run --privileged --rm docker/binfmt
+service docker restart
