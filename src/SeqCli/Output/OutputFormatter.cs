35f8f96af93adf5a3fc58eac855b7fd39a3cbbfe
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-01-30 10:37:19 +1000

    Use Serilog.Expressions where knowledge of the built-in @sp property is needed

--- src/SeqCli/Output/OutputFormatter.cs
+++ src/SeqCli/Output/OutputFormatter.cs
@@ -1,12 +1,15 @@
 using SeqCli.Ingestion;
 using SeqCli.Levels;
 using Serilog.Formatting;
-using Seq.Syntax.Templates;
+using Serilog.Templates;
 
 namespace SeqCli.Output;
 
 static class OutputFormatter
 {
+    // This is the only usage of Serilog.Expressions remaining in seqcli; the upstream Seq.Syntax doesn't yet support
+    // the `@sp` property, because it needs to load on older Seq installs with older Serilog versions embedded in the
+    // app runner. Once we've updated it, we can switch this to a Seq.Syntax template.
     internal static readonly ITextFormatter Json = new ExpressionTemplate(
         $"{{ {{@t, @mt, @l: coalesce({LevelMapping.SurrogateLevelProperty}, if @l = 'Information' then undefined() else @l), @x, @sp, @tr, @ps: coalesce({TraceConstants.ParentSpanIdProperty}, @ps), @st: coalesce({TraceConstants.SpanStartTimestampProperty}, @st), ..rest()}} }}\n"
     );
