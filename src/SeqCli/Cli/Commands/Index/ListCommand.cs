63f414aadea41b068dfd79d01bd7bd3736dfdaff
Liam McLennan <liam.mclennan@gmail.com>
2024-04-19 16:00:56 +1000

    stubbed index commands

---
+++ src/SeqCli/Cli/Commands/Index/ListCommand.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2018 Datalust Pty Ltd and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SeqCli.Cli.Features;
+using SeqCli.Config;
+using SeqCli.Connection;
+
+namespace SeqCli.Cli.Commands.Index;
+
+[Command("index", "list", "List indexes", Example="seqcli index list")]
+class ListCommand : Command
+{
+    readonly SeqConnectionFactory _connectionFactory;
+
+    readonly ConnectionFeature _connection;
+    readonly OutputFormatFeature _output;
+
+    public ListCommand(SeqConnectionFactory connectionFactory, SeqCliConfig config)
+    {
+        if (config == null) throw new ArgumentNullException(nameof(config));
+        _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+
+        _output = Enable(new OutputFormatFeature(config.Output));
+        _connection = Enable<ConnectionFeature>();
+    }
+
+    protected override async Task<int> Run()
+    {
+        var connection = _connectionFactory.Connect(_connection);
+
+        // var list = _userIdentity.Id != null ?
+        //     new[] { await connection.Users.FindAsync(_userIdentity.Id) } :
+        //     (await connection.Users.ListAsync())
+        //     .Where(u => _userIdentity.Name == null || _userIdentity.Name == u.Username);
+        //
+        // _output.ListEntities(list);
+        
+        await Task.Delay(1);
+        return 0;
+    }
+}
---
+++ src/SeqCli/Cli/Commands/Index/SuppressCommand.cs
@@ -0,0 +1,59 @@
+// Copyright 2018 Datalust Pty Ltd and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using SeqCli.Cli.Features;
+using SeqCli.Config;
+using SeqCli.Connection;
+using Serilog;
+
+namespace SeqCli.Cli.Commands.Index;
+
+[Command("index", "suppress", "Suppress index", Example="seqcli index suppress -i signal-6543")]
+class SuppressCommand : Command
+{
+    readonly SeqConnectionFactory _connectionFactory;
+    readonly ConnectionFeature _connection;
+    string? _id;
+
+    public SuppressCommand(SeqConnectionFactory connectionFactory, SeqCliConfig config)
+    {
+        if (config == null) throw new ArgumentNullException(nameof(config));
+        _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+
+        Options.Add(
+            "i=|id=",
+            "The id of an entity with an index to suppress",
+            id => _id = id);
+        
+        _connection = Enable<ConnectionFeature>();
+    }
+
+    protected override async Task<int> Run()
+    {
+        if (_id == null)
+        {
+            Log.Error("An `id` must be specified");
+            return 1;
+        }
+
+        // var connection = _connectionFactory.Connect(_connection);
+        // var toRemove = await connection.RetentionPolicies.FindAsync(_id);
+        // await connection.RetentionPolicies.RemoveAsync(toRemove);
+
+        await Task.Delay(1);
+        return 0;
+    }
+}
