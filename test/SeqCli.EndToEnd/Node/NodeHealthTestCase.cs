dc131b41f6b3c53b68099125db824fc35602306d
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-07-09 15:22:19 +1000

    Fix test

--- test/SeqCli.EndToEnd/Node/NodeHealthTestCase.cs
+++ test/SeqCli.EndToEnd/Node/NodeHealthTestCase.cs
@@ -6,14 +6,14 @@ using Xunit;
 
 namespace SeqCli.EndToEnd.Node;
 
-[CliTestCase(MinimumApiVersion = "2021.3.6410")]
+[CliTestCase]
 public class NodeHealthTestCase: ICliTestCase
 {
     public Task ExecuteAsync(SeqConnection connection, ILogger logger, CliCommandRunner runner)
     {
         var exit = runner.Exec("node health");
         Assert.Equal(0, exit);
-        Assert.StartsWith("HTTP 1.1 200 OK", runner.LastRunProcess!.Output);
+        Assert.StartsWith("{\"status\":", runner.LastRunProcess!.Output);
         return Task.CompletedTask;
     }
 }
