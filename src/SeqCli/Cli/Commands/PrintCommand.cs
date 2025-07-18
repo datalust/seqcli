bc82148bbbb9f9455ded4e633182668b90e76c88
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2025-07-18 17:15:48 +1000

    Fix print command

--- src/SeqCli/Cli/Commands/PrintCommand.cs
+++ src/SeqCli/Cli/Commands/PrintCommand.cs
@@ -34,16 +34,13 @@ class PrintCommand : Command
 {
     readonly FileInputFeature _fileInputFeature;
     readonly InvalidDataHandlingFeature _invalidDataHandlingFeature;
+    readonly StoragePathFeature _storage;
 
     string? _filter, _template = OutputFormat.DefaultOutputTemplate;
-    bool _noColor, _forceColor;
+    bool? _noColor, _forceColor;
 
-    public PrintCommand(SeqCliOutputConfig seqCliOutputConfig)
+    public PrintCommand()
     {
-        if (seqCliOutputConfig == null) throw new ArgumentNullException(nameof(seqCliOutputConfig));
-        _noColor = seqCliOutputConfig.DisableColor;
-        _forceColor = seqCliOutputConfig.ForceColor;
-
         _fileInputFeature = Enable(new FileInputFeature("CLEF file to read", allowMultiple: true));
 
         Options.Add("f=|filter=",
@@ -56,20 +53,24 @@ class PrintCommand : Command
 
         _invalidDataHandlingFeature = Enable<InvalidDataHandlingFeature>();
 
+        // These should be ported to use `OutputFormatFeature`.
         Options.Add("no-color", "Don't colorize text output", _ => _noColor = true);
-
         Options.Add("force-color",
             "Force redirected output to have ANSI color (unless `--no-color` is also specified)",
             _ => _forceColor = true);
+
+        _storage = Enable<StoragePathFeature>();
     }
 
     protected override async Task<int> Run()
     {
+        var config = RuntimeConfigurationLoader.Load(_storage);
+        
         var applyThemeToRedirectedOutput
-            = !_noColor && _forceColor;
+            = !(_noColor ?? config.Output.DisableColor) && (_forceColor ?? config.Output.ForceColor);
 
         var theme
-            = _noColor                      ? ConsoleTheme.None
+            = _noColor ?? config.Output.DisableColor                      ? ConsoleTheme.None
             :  applyThemeToRedirectedOutput ? OutputFormat.DefaultAnsiTheme
             :                                 OutputFormat.DefaultTheme;
 
