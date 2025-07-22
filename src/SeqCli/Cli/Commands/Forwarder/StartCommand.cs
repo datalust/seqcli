330dceb586403dd9401b9135e1def64e1f18f124
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2025-07-23 09:31:48 +1000

    Switch `config` command to a command group, including `clear`, `get`, `set`, and `list` to match Seq's CLI

--- src/SeqCli/Cli/Commands/Forwarder/StartCommand.cs
+++ src/SeqCli/Cli/Commands/Forwarder/StartCommand.cs
@@ -22,7 +22,7 @@ using SeqCli.Forwarder.ServiceProcess;
 
 namespace SeqCli.Cli.Commands.Forwarder;
 
-[Command("forwarder", "start", "Start the forwarder Windows service", IsPreview = true)]
+[Command("forwarder", "start", "Start the forwarder Windows service", Visibility = FeatureVisibility.Preview)]
 [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 class StartCommand : Command
 {
