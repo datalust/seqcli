2b4366479e1b2deb9ef176c27bb9100d3c4373ed
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2026-05-08 08:55:12 +1000

    Update inconsistent copyright headers

--- src/SeqCli/Cli/Commands/LogCommand.cs
+++ src/SeqCli/Cli/Commands/LogCommand.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-// Copyright 2018 Datalust Pty Ltd
+// Copyright © Datalust and contributors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
